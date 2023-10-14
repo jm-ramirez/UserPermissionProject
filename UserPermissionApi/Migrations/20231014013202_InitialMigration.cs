0282eae6789fff0518e334f2b106ae2dd7f9b42f
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2023-10-13 22:32:54 -0300

    Add Initial Migration.

---
+++ UserPermissionApi/Migrations/20231014013202_InitialMigration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserPermissionApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PermissionTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreEmpleado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApellidoEmpleado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaPermiso = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TipoPermiso = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Permissions_PermissionTypes_TipoPermiso",
+                        column: x => x.TipoPermiso,
+                        principalTable: "PermissionTypes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissions_TipoPermiso",
+                table: "Permissions",
+                column: "TipoPermiso");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "PermissionTypes");
+        }
+    }
+}
