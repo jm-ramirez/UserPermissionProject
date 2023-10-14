ed0d4de8d59a69993c5b0cbc26bea79337a2b371
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2023-10-13 22:58:28 -0300

    Create unit test project.

---
+++ UserPermissionUnitTest/RequestPermissionServiceTests.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using UserPermissionApi.Model;
+
+namespace UserPermissionUnitTest
+{
+    //public class RequestPermissionServiceTests
+    //{
+    //    [Fact]
+    //    public void ProcessPermissionRequest_Should_Return_PermissionApproved()
+    //    {
+    //        // Arrange
+    //        var requestPermissionService = new RequestPermissionController(); // Puedes utilizar inyección de dependencias o mocks en lugar de instanciar directamente
+
+    //        // Crea una solicitud de permiso de ejemplo
+    //        var permissionRequest = new PermissionRequest
+    //        {
+    //            EmployeeId = 123,
+    //            PermissionType = PermissionTypes.Request, // Ajusta según tu enumeración de tipos de permiso
+    //            RequestDetails = "Razón de la solicitud",
+    //            // Otros campos requeridos para la solicitud
+    //        };
+
+    //        // Act
+    //        var result = requestPermissionService.ProcessPermissionRequest(permissionRequest);
+
+    //        // Assert
+    //        // Verifica que el resultado sea igual a "PermissionApproved" (supongamos que este es un valor esperado)
+    //        Assert.Equal(PermissionStatus.PermissionApproved, result);
+    //    }
+    //}
+}
---
+++ UserPermissionUnitTest/UpdatePermissionServiceTests.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using UserPermissionApi.Model;
+
+namespace UserPermissionUnitTest
+{
+    //public class UpdatePermissionServiceTests
+    //{
+    //    [Fact]
+    //    public void UpdatePermission_Should_Update_PermissionStatus()
+    //    {
+    //        // Arrange
+    //        var updatePermissionService = new UpdatePermissionService(); // Puedes utilizar inyección de dependencias o mocks en lugar de instanciar directamente
+
+    //        // Crea un permiso existente de ejemplo con un estado inicial
+    //        var existingPermission = new Permissions
+    //        {
+    //            Id = 123,
+    //            PermissionStatus = PermissionStatus.Requested, // Supongamos que este es el estado inicial
+    //                                                           // Otros campos requeridos para el permiso
+    //        };
+
+    //        // Definir el nuevo estado al que se actualizará el permiso
+    //        var newPermissionStatus = PermissionStatus.Approved;
+
+    //        // Act
+    //        var updatedPermission = updatePermissionService.UpdatePermission(existingPermission, newPermissionStatus);
+
+    //        // Assert
+    //        // Verificar que el estado del permiso actualizado sea igual al nuevo estado esperado
+    //        Assert.Equal(newPermissionStatus, updatedPermission.PermissionStatus);
+    //    }
+    //}
+}
