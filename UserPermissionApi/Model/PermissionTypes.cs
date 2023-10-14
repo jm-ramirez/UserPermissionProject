53ef694a4fdb18a29b8e06a474c800c388a5019c
Juan Manuel Ramirez <juanchiramirez5@gmail.com>
2023-10-13 22:25:09 -0300

    I create models and install necessary libraries.

---
+++ UserPermissionApi/Model/PermissionTypes.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserPermissionApi.Model
+{
+    public class PermissionTypes
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Description("Unique ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Description("Permission description")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        public ICollection<Permissions>? Permissions { get; set; } // Propiedad de navegación inversa
+    }
+}
---
+++ UserPermissionApi/Model/Permissions.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserPermissionApi.Model
+{
+    public class Permissions
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Description("Unique ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Description("Employee Forename")]
+        public string NombreEmpleado { get; set; } = string.Empty;
+
+        [Required]
+        [Description("Employee Surname")]
+        public string ApellidoEmpleado { get; set; } = string.Empty;
+
+        [Required]
+        [Description("Permission granted on Date")]
+        public DateTime FechaPermiso { get; set; }
+
+        [Required]
+        [Description("Permission Type")]
+        public int TipoPermiso { get; set; } // Clave foránea
+        public PermissionTypes? PermissionTypes { get; set; } // Propiedad de navegación
+    }
+}
